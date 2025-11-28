b0f3550f86128975f002cadf17458e3afa17b659
AndriiDrahniew <qwedfgvbn13122002@gmail.com>
2025-11-28 16:27:36 +0100

    Fixed /src/routes/v1/index.ts

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,9 +3,17 @@ import { Router } from 'express';
 import auth from './auth';
 import users from './users';
 
+import horses from './horses';
+import trainingAplications from './TrainingAplications';
+import tradeReports from './TradeReports';
+
 const router = Router();
 
 router.use('/auth', auth);
 router.use('/users', users);
+
+router.use('/horses', horses);
+router.use('/training-aplications', trainingAplications);
+router.use('/trade-reports', tradeReports);
 
 export default router;
